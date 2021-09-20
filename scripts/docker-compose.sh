20beaeba3ed4c9d219123e2194a4e6b7806d64e2
Riki Permana <arwahdevops@gmail.com>
2021-09-21 01:35:42 +0700

    add ami 2 docker installer

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Amazon Linux 2 install docker
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
-sudo yum install -y git
+# Amazon Linux 2 install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
